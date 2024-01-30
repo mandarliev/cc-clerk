617a6110e2ecc4efd80ca979b0e62c8bb05a5f6e
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-30 13:32:03 +0200

    enough

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 import { getAuth } from "@clerk/nextjs/server";
+import { clerkClient } from "@clerk/nextjs";
 
 export async function GET(request: NextRequest) {
   const { userId, sessionId } = getAuth(request);
@@ -8,6 +9,9 @@ export async function GET(request: NextRequest) {
     NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     return;
   }
+
+  const user = userId ? await clerkClient.users.getUser(userId) : null;
+  console.log(user);
 
   return NextResponse.json({ data: "Hello World", userId, sessionId });
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Profile from "@/components/Profile";
 import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
+  const { userId }: { userId: string | null } = auth();
+
+  if (!user) return <div>Not logged in</div>;
   return (
     <main>
-      <h1>Clerk Authentication Demo</h1>
+      <h1>Hello World - (user is {user?.firstName})</h1>
       <UserButton afterSignOutUrl="/" />
       <Profile />
     </main>
